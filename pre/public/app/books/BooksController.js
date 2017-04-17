d958e44c1529b3596bc2af3cf9bf23a91fc0559f
marek.kacprzak <marek.kacprzak@windowslive.com>
2017-04-17 11:50:45 +0200

    Value service sample - function with parameter - injected into controller, exposed in vm and used in view html binding

--- pre/public/app/books/BooksController.js
+++ pre/public/app/books/BooksController.js
@@ -1,18 +1,18 @@
 (function() {
-
+    "use strict";
     angular.module('app')
         .controller('BooksController',  BooksController);
 
-
-    function BooksController(books, dataService, logger) {
-
+    function BooksController(books, dataService, logger, badgeService) {
         var vm = this;
         vm.appName = books.appName;
         vm.allBooks = dataService.getAllBooks();
+        vm.allReaders = dataService.getAllReaders();
+        vm.getBadge = badgeService.retrieveBadge;
         logger.output('BooksController has been created.')
         vm.allBooks.forEach(function (element) {
             logger.logBook(element);
         });
     }
-    
+
 }());
