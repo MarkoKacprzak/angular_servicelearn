21efe5124911e1e10fa04d956c0822ff24723a5f
marek.kacprzak <marek.kacprzak@windowslive.com>
2017-04-17 09:26:11 +0200

    provider without config - define from module

--- pre/public/app/app.js
+++ pre/public/app/app.js
@@ -1,19 +1,14 @@
 (function () {
     "use strict";   
     var app = angular.module('app', []);
-
-    app.config(function ($provide) {
-        
-        $provide.provider('books', function () {
-            this.$get = function () {
-                var appName = 'Book Logger';
-                var appDesc = 'Track which books you read.';
-
-                return {
-                    appName: appName,
-                    appDesc: appDesc
-                };
+    app.provider('books', function () {
+        this.$get = function () {
+            var appName = 'Book Logger';
+            var appDesc = 'Track which books you read.';
+            return {
+                appName: appName,
+                appDesc: appDesc
             };
-        });
+        };
     });
 }());
